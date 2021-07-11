21a7377034b08b286176f62a021d6a2f8909b55a
caotv <caotv1d@gmail.com>
2021-07-11 09:46:01 +0700

    Creating a Container Component

--- src/screens/Contacts/index.js
+++ src/screens/Contacts/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import Container from '../../components/common/container';
 
 const Contacts = () => {
     return (
-        <View>
+        <Container>
             <Text>Contacts page!</Text>
-        </View>
+        </Container>
     );
 }
 
