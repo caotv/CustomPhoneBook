97eb760f8600c026cfe6f1fe6807781e36c3b2e3
caotv <caotv1d@gmail.com>
2021-07-18 20:03:29 +0700

    Message component

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,21 +1,26 @@
 import React from 'react';
-import {useNavigation} from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native';
 import { TouchableOpacity, Text, Image, View } from 'react-native';
 import Container from '../../components/common/container';
 import CustomButton from '../../components/common/CustomButton';
 import Input from '../../components/common/Input';
 import styles from './styles';
 import { REGISTER } from '../../constants/routeName';
+import Message from '../common/Message';
 
 
 const LoginComponent = () => {
-    const {navigate} = useNavigation();
+    const { navigate } = useNavigation();
     return (
         <Container>
             <Image source={require('../../../src/assets/images/logo.png')} style={styles.logoImage} />
             <View>
                 <Text style={styles.title}>Wellcome to RNContacts</Text>
                 <Text style={styles.subTitle}>Please login here</Text>
+                <Message retry onDismiss= {() => {}} message="Invalid credential" primary/>
+                <Message onDismiss= {() => {}} message="Invalid credential" danger/>
+                <Message onDismiss= {() => {}} message="Invalid credential" info/>
+                <Message onDismiss= {() => {}} message="Invalid credential" success/>
 
                 <Input
                     label="User name"
@@ -36,7 +41,7 @@ const LoginComponent = () => {
             <View style={styles.createSection}>
                 <Text style={styles.infoText}>Need a new account?</Text>
                 <TouchableOpacity
-                    onPress = {() => {
+                    onPress={() => {
                         navigate(REGISTER);
                     }}
                 >
---
+++ src/components/common/Message/index.js
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import colors from '../../../assets/theme/colors';
+import styles from './styles';
+
+const Message = ({
+    primary,
+    danger,
+    success,
+    disabled,
+    message,
+    info,
+    retry,
+    retryFn,
+    onDismiss,
+}) => {
+
+    const [userDismissed, setDismissed] = useState(false);
+
+    const getFlexDirection = () => {
+        if (icon && iconPosition) {
+            if (iconPosition === "left") {
+                return 'row';
+            } else if (iconPosition === 'right') {
+                return 'row-reverse';
+            }
+        }
+    };
+
+    const getBgColor = () => {
+        if (disabled) {
+            return colors.grey;
+        }
+        if (primary) {
+            return colors.primary;
+        }
+        if (success) {
+            return colors.success;
+        }
+        if (danger) {
+            return colors.danger;
+        }
+        if (info) {
+            return colors.secondary;
+        }
+    };
+
+    return (
+        <>
+            {userDismissed ? null :
+                < TouchableOpacity
+                    style={[styles.wrapper, { backgroundColor: getBgColor() }]}
+                >
+                    <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                        <Text style={{ color: colors.white }}>
+                            {message}
+                        </Text>
+                        {retry && !typeof onDismiss === "function" && (
+                            <TouchableOpacity onPress={retryFn}>
+                                <Text style={{ color: colors.white }}>Retry</Text>
+                            </TouchableOpacity>
+                        )}
+
+                        {typeof onDismiss === "function" && (
+                            <TouchableOpacity onPress={() => {
+                                setDismissed(true);
+                                onDismiss();
+                            }}>
+                                <Text style={{ color: colors.white }}>X</Text>
+                            </TouchableOpacity>
+                        )}
+                    </View>
+                </TouchableOpacity>
+            }
+        </>
+    );
+};
+
+export default Message;
