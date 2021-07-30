31655410dc2d82c2690760f71bc67763a1d7da2a
caotv <caotv1d@gmail.com>
2021-07-30 18:25:00 +0700

    Root navigation & Axios auth interceptor

--- src/navigation/HomeNavigator.js
+++ src/navigation/HomeNavigator.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import { CONTACT_DETAIL, CONTACT_LIST, CREATE_CONTACT, SETTINGS } from '../constants/routeName';
+import { CONTACT_DETAIL, CONTACT_LIST, CREATE_CONTACT, SETTINGS, LOGOUT } from '../constants/routeName';
 import Contacts from '../screens/Contacts';
 import ContactDetails from '../screens/ContactDetails';
 import CreateContact from '../screens/CreateContact';
 import Settings from '../screens/Settings';
 import { Text } from 'react-native';
+import Logout from '../screens/Logout';
 
-
+r
 const HomeNavigator = () => {
 
     const HomeStack = createStackNavigator();
@@ -17,6 +18,7 @@ const HomeNavigator = () => {
             <HomeStack.Screen name={CONTACT_DETAIL} component={ContactDetails} />
             <HomeStack.Screen name={CREATE_CONTACT} component={CreateContact} />
             <HomeStack.Screen name={SETTINGS} component={Settings} />
+            <HomeStack.Screen name={LOGOUT} component={Logout} />
         </HomeStack.Navigator>
     );
 }
